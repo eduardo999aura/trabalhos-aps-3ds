65dda376ce558bc12cb39658c576e2c1b94c9f1e
eduardo999aura <eduardo.penerotti.dias@escola.pr.gov.br>
2026-09-09 08:01:12 -0300

    adicionando projeto 1

---
+++ main.py
@@ -0,0 +1,12 @@
+class Aluno:
+
+    def__init__(self, nome, idade, curso):
+        self.nome = nome 
+        self.idade = idade
+        self.curso = curso
+
+aluno1 = Aluno("João", 13, "D.S")
+print(aluno1,nome)
+print(aluno1,idade)
+aluno2 = Aluno("Adenilson", 58, "D.S")
+print(aluno2.nome)
